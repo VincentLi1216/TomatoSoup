62dbe8b14a564224ce9feede9227cd19ab4ba943
Lyz23 <LS0723000@gmail.com>
2022-09-26 00:22:23 +0800

    recreate a new android app project

--- Android-App/ktor-tomatosoup/build.gradle.kts
+++ Android-App/ktor-tomatosoup/build.gradle.kts
@@ -1,5 +1,6 @@
 val ktor_version: String by project
 val kotlin_version: String by project
+val gson_version: String by project
 val logback_version: String by project
 
 plugins {
@@ -24,7 +25,10 @@ repositories {
 dependencies {
     implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
+    implementation("com.google.code.gson:gson:$gson_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("io.ktor:ktor-server-content-negotiation-jvm:2.1.1")
+    implementation("io.ktor:ktor-serialization-gson-jvm:2.1.1")
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
--- Android-App/ktor-tomatosoup/src/main/kotlin/soup/example/plugins/Routing.kt
+++ Android-App/ktor-tomatosoup/src/main/kotlin/soup/example/plugins/Routing.kt
@@ -1,16 +1,22 @@
 package soup.example.plugins
 
-import io.ktor.server.routing.*
 import io.ktor.http.*
+import io.ktor.serialization.gson.*
 import io.ktor.server.application.*
-import io.ktor.server.response.*
+import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
-
+    install(ContentNegotiation) {
+        gson {
+        }
+    }
     routing {
         get("/") {
-            call.respondText("Hello World!")
+            call.respond(mapOf("message" to "HELLO WORLD!"))
         }
+
     }
 }
