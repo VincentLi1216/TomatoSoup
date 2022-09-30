82b5957b5753c72b0857ac103e7c2403d5a4ed59
VincentLi1216 <sunnus.tw@gmail.com>
2022-09-30 14:40:30 +0800

    Hi

--- ImageProcessing/main.py
+++ ImageProcessing/main.py
@@ -1,15 +1,7 @@
 from util_dst_cs_M import corner_selector
-<<<<<<< HEAD
-from perspective_transform import perspective_transform
 
-# print(corner_selector())
-corner_selector()
-=======
-from crop_blackboard import *
 
 
 print(corner_selector())
 
-# file_name = "FFC01EC6-E6F4-4FFF-BBD8-DFC4D0E0A6E1-16.9"
-# crop_blackboard(file_name)
->>>>>>> f05c32838bba837277698a1e043c83d3a10b5b8d
+
