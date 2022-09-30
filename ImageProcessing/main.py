39d8538237697d7fbfc2d5be3dd871bdffd5ebf7
allen327lin <allen327lin2@gmail.com>
2022-09-30 15:33:17 +0800

    modify main

--- ImageProcessing/main.py
+++ ImageProcessing/main.py
@@ -1,5 +1,8 @@
-from util_manual_corner_select import corner_selector
+from util_houghlines_blackboard import *
 
-
-
-print(corner_selector())
+file_name = "FFC01EC6-E6F4-4FFF-BBD8-DFC4D0E0A6E1-16.9"
+img_to_houghlines = cv.imread("developing_images\\" + f'{file_name}' + ".jpg", 1)
+result_img = houghlines_blackboard(img_to_houghlines)
+cv2.imshow("result_img", result_img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
