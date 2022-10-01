1e65b6c846cff1113bbf6173c7f9eb31230702e4
allen327lin <allen327lin2@gmail.com>
2022-10-01 19:10:06 +0800

    fix error indent

--- ImageProcessing/util_grab_images.py
+++ ImageProcessing/util_grab_images.py
@@ -1,18 +1,18 @@
 import cv2
 
 cv2.namedWindow("taking shots")
-  vc = cv2.VideoCapture(0)
-  vc.set(cv2.CAP_PROP_FRAME_WIDTH, 2160)
-  vc.set(cv2.CAP_PROP_FRAME_HEIGHT, 1440)
+vc = cv2.VideoCapture(0)
+vc.set(cv2.CAP_PROP_FRAME_WIDTH, 2160)
+vc.set(cv2.CAP_PROP_FRAME_HEIGHT, 1440)
 
+rval, frame = vc.read()
+
+while True:
+
+  if frame is not None:
+    cv2.imshow("window1", frame)
   rval, frame = vc.read()
 
-  while True:
+  if cv2.waitKey(1) & 0xFF == ord('q'):
 
-    if frame is not None:
-      cv2.imshow("window1", frame)
-    rval, frame = vc.read()
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-
-      break
+    break
