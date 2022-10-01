76a8aaec3b4f1d17f229c3e02c47d5c4678bd389
allen327lin <allen327lin2@gmail.com>
2022-10-01 19:35:07 +0800

    main_allen modified

--- ImageProcessing/main_allen.py
+++ ImageProcessing/main_allen.py
@@ -1,3 +1,4 @@
+from util_distortion_correction import *
 from util_houghlines_blackboard import *
 
 file_name = return_time()
@@ -5,29 +6,7 @@ file_name = return_time()
 # img_to_houghlines = cv.imread("developing_images\\" + f'{file_name}' + ".jpg", 1)
 
 
-cv2.namedWindow("window1")
-
-input_camera = None  # user enter camera index
-while input_camera == None or int(input_camera) > 3 or int(input_camera) < 0:
-    input_camera = int(input("Please enter the camera you would like to use(0~3):"))
-
-vc = cv2.VideoCapture(input_camera)
-
-vc.set(cv2.CAP_PROP_FRAME_WIDTH, 3840)
-vc.set(cv2.CAP_PROP_FRAME_HEIGHT, 2160)
-
-rval, frame = vc.read()
-
-while True:
-
-    if frame is not None:
-        cv2.imshow("window1", frame)
-    rval, frame = vc.read()
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-
+frame = distorion_correction()
 result_img = houghlines_blackboard(file_name, frame)
 
 
@@ -41,7 +20,7 @@ result_img = houghlines_blackboard(file_name, frame)
 
 
 
-# cv2.imshow("result_img", result_img)
+# cv2.imshow("result_img", frame)
 # cv2.waitKey(0)
 # cv2.destroyAllWindows()
 
