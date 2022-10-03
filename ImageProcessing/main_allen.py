d6984d46d0c6e56932e1e0f6f6a91faa59f8746e
allen327lin <allen327lin2@gmail.com>
2022-10-03 22:51:17 +0800

    add firebase_communicator.py

--- ImageProcessing/main_allen.py
+++ ImageProcessing/main_allen.py
@@ -1,13 +1,13 @@
-from util_distortion_correction import *
-from util_houghlines_blackboard import *
+# from util_distortion_correction import *
+# from util_houghlines_blackboard import *
 
-file_name = return_time()
+# file_name = return_time()
 # file_name = "FFC01EC6-E6F4-4FFF-BBD8-DFC4D0E0A6E1-16.9"
 # img_to_houghlines = cv.imread("developing_images\\" + f'{file_name}' + ".jpg", 1)
 
 
-frame = distorion_correction()
-result_img = houghlines_blackboard(file_name, frame)
+# frame = distorion_correction()
+# result_img = houghlines_blackboard(file_name, frame)
 
 
 
@@ -24,3 +24,10 @@ result_img = houghlines_blackboard(file_name, frame)
 # cv2.waitKey(0)
 # cv2.destroyAllWindows()
 
+
+
+
+
+from firebase_communicator import *
+
+print("stopped")
